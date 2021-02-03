8ec33e7af72b34ede208d5cbaedb4f3089f8a1e5
enjoyee <kwqjk03@126.com>
2021-02-03 10:46:38 +0800

    update

--- uutil/src/main/java/com/glimmer/uutil/KHandler.kt
+++ uutil/src/main/java/com/glimmer/uutil/KHandler.kt
@@ -1,12 +1,13 @@
 package com.glimmer.uutil
 
 import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 
-class KHandler(private val lifecycleOwner: LifecycleOwner, callback: Callback? = null) : Handler(callback), LifecycleObserver {
+class KHandler(private val lifecycleOwner: LifecycleOwner, callback: Callback? = null, looper: Looper = Looper.getMainLooper()) : Handler(looper, callback), LifecycleObserver {
 
     init {
         lifecycleOwner.lifecycle.addObserver(this)
